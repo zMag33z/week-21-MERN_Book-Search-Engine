3f440862387ea16b47d44d2e5c88a92e941843db
zMag33z <mag33.cdh@gmail.com>
2023-04-07 21:45:00 -0500

    fixing bookadd

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,8 +25,8 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookToSave: bookInput) {
-    saveBook(bookToSave: $bookToSave) {
+  mutation saveBook($newBook: BookInput!) {
+    saveBook(newBook: $newBook) {
       username
       email
       bookCount
