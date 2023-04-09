9a1da194bdc29d12190c6cd76bdaba5cd4a4b23e
zMag33z <mag33.cdh@gmail.com>
2023-04-09 10:38:48 -0500

    fixed key to children in jsx component

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 // for two days i've been trying to figure this out, while taking a break and an epiphany struck - localhost needs to change to 127.0.0.1:27017  OMG
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,  console says depreciated will not run removed runs then waits and then drops again
-  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
